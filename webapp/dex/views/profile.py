8682b4dd5ddab1e0def64e168f57e9d8875ff057
Roger Dahl <git@dahlsys.com>
2020-12-18 13:06:14 -0700

    Start generating profile without requiring click. Add simple animation and elapsed time.

--- webapp/dex/views/profile.py
+++ webapp/dex/views/profile.py
@@ -1,11 +1,6 @@
-"""
-"""
-
 import logging
-import pathlib
 import shlex
 import subprocess
-import sys
 import time
 
 import flask
@@ -23,28 +18,18 @@ profile_blueprint = flask.Blueprint(
 
 @profile_blueprint.route("/<rid>")
 def profile(rid):
-    if dex.cache.is_cached(rid, "profile", "html"):
-        iframe_src = f"/dex/profile/doc/{rid}"
-    else:
-        iframe_src = f"/dex/profile/generate/{rid}"
     return flask.render_template(
         "profile.html",
         rid=rid,
         entity_tup=db.get_entity_as_dict(rid),
-        iframe_src=iframe_src,
-    )
-
-
-@profile_blueprint.route("/generate/<rid>")
-def generate(rid):
-    return flask.render_template(
-        "profile-generate.html", rid=rid, entity_tup=db.get_entity_as_dict(rid)
+        is_cached='true' if dex.cache.is_cached(rid, "profile", "html") else 'false',
     )
 
 
 @profile_blueprint.route("/doc/<rid>")
 def doc(rid):
-    render_profile(rid)
+    if not dex.cache.is_cached(rid, "profile", "html"):
+        render_profile(rid)
     return flask.send_file(dex.cache.get_cache_path(rid, "profile", "html"))
 
 
@@ -68,41 +53,10 @@ def render_profile(rid):
     html_bytes = subprocess.check_output(cmd_list)
     webapp.perf.set(f"{rid}/profile-sec", time.time() - start_ts)
     log.debug('html_bytes=({}) "{}"'.format(len(html_bytes), html_bytes[:100]))
-    return html_bytes.decode("utf-8")
+
     # cache_path = dex.cache.get_cache_path(
     #     rid, 'profile', 'html', mkdir=True
     # )
     # cache_path.write_bytes(html_bytes)
 
-
-# @profile_blueprint.route('/profile-fetch/<rid>')
-# def profile_render(rid):
-#     return flask
-#     csv_path = csv_cache.get_csv_path(rid)
-#     cmd_list = [
-#         # For debugging in PyCharm, run pandas-profiling in a separate process. The
-#         # python process is wrapped in a shell script to prevent the debugger from
-#         # attaching to it. The process crashes if the debugger is able to attach to it.
-#         # Turning off automatic attach in the PyCharm debugger settings causes the
-#         # debugger to trigger a crash in Flask.
-#         flask.current_app.config['PROFILING_SH'].as_posix(),
-#         sys.executable,
-#         csv_path.resolve().as_posix(),
-#         # sys.executable,
-#         # flask.current_app.config['PROFILING_BIN'].as_posix(),
-#         # csv_path.resolve().as_posix(),
-#     ]
-#     log.debug(
-#         'Running: {}'.format(' '.join([shlex.quote(s) for s in cmd_list]))
-#     )
-#     start_ts = time.time()
-#     html_bytes = subprocess.check_output(cmd_list)
-#     webapp.perf.set(f'{rid}/profile-sec', time.time() - start_ts)
-#     log.debug('html_bytes=({}) "{}"'.format(len(html_bytes), html_bytes[:100]))
-#     cache_path = flask.current_app.config['CACHE_ROOT_DIR'] / rid
-#     cache_path.parent.mkdir(exist_ok=True)
-#     cache_path.write_bytes(html_bytes)
-#     return flask.send_file(cache_path, mimetype='text/html', )
-#
-#     # return cache_path
-#     # return html_bytes
+    return html_bytes.decode("utf-8")
--- webapp/static/profile.js
+++ webapp/static/profile.js
@@ -1,23 +1,10 @@
-// let seconds = 0;
-//
-// $(document).ready(function () {
-//   iFrameResize({ log: true }, '#profile');
-// });
-
-//   $('#generate').click(async function () {
-//     $(this).css("display", "none");
-//     $('#spinner-msg').css("display", "block");
-//     setInterval(elapsedCounter, 1000);
-//     // let response = await fetch(`/dex/profile-fetch/${g.rid}`);
-//     // document.open('text/html');
-//     // document.write(await response.text());
-//     $('#profile').src = `/dex/profile/profile-fetch/${g.rid}`
-//     $('#profile').load(function(){$(this).height($(this).contents().outerHeight());});
-//
-//   });
-// });
-//
-// function elapsedCounter() {
-//   seconds += 1;
-//   $('#counter-msg').text(`Elapsed: ${seconds}s`);
-// }
+$(document).ready(
+    async function () {
+      if (!g.is_cached) {
+        $('#generate-msg').Loading('Generating profile for CSV (may take several minutes)');
+        $('#profile').on('load', function () {
+          $('#generate-msg').Destroy();
+        });
+      }
+    }
+);
