2a873a71b946f1f9043440730265e71934cba005
Roger Dahl <git@dahlsys.com>
2020-12-18 12:24:37 -0700

    Add general solution for displaying message and elapsed time for long running tasks
    - The message has a simple animation, adding and removing periods (ellipsis).
    - Elapsed time is showed in seconds.

--- webapp/static/loading.js
+++ webapp/static/loading.js
@@ -1,161 +1,70 @@
-let start_ts;
-let interval_handle;
+// Display a simple animation indicating that we are waiting for a task to complete.
+//
+// This adds a function, `Loading()` to all elements. Calling Loading() on an element replaces any
+// current contents in the element with a message and a count of elapsed time, in seconds. The
+// message is "Loading" by default and includes a simple animation where trailing dots are added up
+// to MAX_DOTS (5 by default), and then starts over.
+//
+// Typically, the animation is destroyed implicitly when the task for which we are waiting
+// completes, and the current document is replaced or the elements are overwritten. But it's also
+// possible to stop the animation and restore the element to its previous state by calling
+// Destroy().
 
-function start()
-{
-
-}
-
-// language=HTML
 const INJECT_HTML = `
-  <div class='dex-loading'>
+  <div id='loading-outer'>
     <div id='loading-msg'></div>
-    <div id='counter-msg'></div>
+    <div id='loading-elapsed'></div>
   </div>
 `;
 
-// (function ($) {
+$(document).ready(function () {
+  // Add the "Loading()" function to all elements.
+  $.fn.Loading = function (msg_str = 'Loading') {
+    // Calling "Loading()" on an element starts the animation and adds a new function,
+    // "Destroy()" (described above) to the element.
+    $.fn.Destroy = function () {
+      StopAnim($(this)[0]);
+    }
+    return this.each(function () {
+      StartAnim(this, msg_str);
+    })
+  };
+});
 
+// Replace any existing contents in the element with the new, animated message. Keep
+// the old content, for use in Destroy().
+// $.StartAnim = function (host_el, msg_str) {
+function StartAnim(host_el, msg_str) {
+  let g = Object;
+  host_el.g = g;
+  g.origHtml = host_el.innerHTML;
+  host_el.innerHTML = INJECT_HTML;
 
-// $.LoadingDots = function (el, options) {
-//   var base = this;
-//   base.$el = $(el);
-//   base.$el.data("LoadingDots", base);
-//
-//   base.dotItUp = function ($element, maxDots) {
-//     if ($element.text().length == maxDots) {
-//       $element.text("");
-//     } else {
-//       $element.append(".");
-//     }
-//   };
-//
-//   base.stopInterval = function () {
-//     clearInterval(base.theInterval);
-//   };
-//
+  g.msg_str = msg_str;
+  g.start_ts = Date.now();
+  g.interval_handle = setInterval(function () {
+    update(g);
+  }, 500);
+  g.dot_count = 0;
 
-// $.fn.LoadingDots = function (options) {
-//   if (typeof (options) == "string") {
-//     var safeGuard = $(this).data('LoadingDots');
-//     if (safeGuard) {
-//       safeGuard.stopInterval();
-//       this[0].innerHTML = safeGuard.h;
-//     }
-//   }
-//   else {
-//     return this.each(function () {
-//       (new $.LoadingDots(this, options));
-//     });
-//   }
-// };
+  update(g);
+}
 
+// $.StopAnim = function (host_el) {
+function StopAnim(host_el) {
+  clearInterval(host_el.g.interval_handle);
+  host_el.innerHTML = host_el.g.origHtml;
+}
 
-// Add the "Loading()" function to all elements. Calling the function replaces any
-// current contents in the element with a message saying, "Loading..." and a display
-// of elapsed time.
-$.fn.Loading = function (msg_str = 'Loading...') {
-  return this.each(function () {
-    (new $.LoadingDots(this, options));
-  })
-};
-
-$.fn.Destroy = function () {
-  return this.each(function () {
-    (new $.LoadingDots(this, options));
-  })
-};
-
-
-if (typeof (options) == "string") {
-  var safeGuard = $(this).data('LoadingDots');
-  if (safeGuard) {
-    safeGuard.stopInterval();
-    this[0].innerHTML = safeGuard.h;
+function update(base)
+{
+  if (base.dot_count++ === 5) {
+    base.dot_count = 0;
   }
-}
-else {
-}
-
-
-
-$.LoadingDots = function (el, options) {
-  var base = this;
-  base.$el = $(el);
-  base.$el.data("LoadingDots", base);
-
-  base.dotItUp = function ($element, maxDots) {
-    if ($element.text().length === maxDots) {
-      $element.text("");
-    }
-    else {
-      $element.append(".");
-    }
-  };
-
-  base.stopInterval = function () {
-    clearInterval(base.theInterval);
-  };
-
-  base.init = function () {
-    if (typeof (speed) === "undefined" || speed === null) {
-      speed = 300;
-    }
-    if (typeof (maxDots) === "undefined" || maxDots === null) {
-      maxDots = 3;
-    }
-    base.speed = speed;
-    base.maxDots = maxDots;
-    base.options = $.extend({}, $.LoadingDots.defaultOptions, options);
-    // base.origHTML = base.$el.html();
-    // alert(base.origHTML);
-    // alert(base.$el.html());
-    base.h = base.$el.html();
-
-    base.$el.html(
-        "<span class='select2-selection__rendered'>" + base.options.word + "<em></em></span>");
-    base.$dots = base.$el.find("em");
-    base.$loadingText = base.$el.find("span");
-    base.$el.css("position", "relative");
-    base.$loadingText.css({
-      "position": "absolute",
-      "top": (base.$el.outerHeight() / 2) - (base.$loadingText.outerHeight() / 2) + 2,
-      "left": "8px",
-      // "left": (base.$el.width() / 2) - (base.$loadingText.width() / 2)
-    });
-  };
-
-  base.init();
-};
-
-$.LoadingDots.defaultOptions = {
-  speed: 300,
-  maxDots: 3,
-  word: "Loading"
-};
-
-
-// function stop() {
-//
-// }
-
-function start_timer()
-{
-  start_ts = Date.now();
-  interval_handle = setInterval(function () {
-    update_elapsed_sec();
-  }, 1000);
-}
-
-function update_elapsed_sec()
-{
+  $('#loading-msg').html(base.msg_str + '.'.repeat(base.dot_count));
   // Since this method is called at the full second mark, we could end up randomly
   // switching to displaying the new value just before or just after it has changed,
   // which would cause the counter stay at a value for an extra second or to skip
   // a value. To avoid that, we add 500 ms and then round the resulting elapsed time.
-  $('#counter-msg').text(`Elapsed: ${Math.round(Date.now() - start_ts + 500)}s`);
+  $('#loading-elapsed').text(`Elapsed: ${Math.round((Date.now() - base.start_ts + 500) / 1000)}s`);
 }
-
-
-// })(jQuery);
-
