e614a7df4d39f81586e7577210dc3bfc9d790c0f
Roger Dahl <git@dahlsys.com>
2021-05-13 11:19:11 -0600

    Add caching for EML etree objects

--- webapp/dex/eml_cache.py
+++ webapp/dex/eml_cache.py
@@ -1,24 +1,19 @@
 """PASTA and EML utils"""
 
-import contextlib
 import datetime
-import io
 import logging
-import pathlib
-import types
 
-import dateutil.parser
 import lxml.etree
-import pygments
-import pygments.formatters
-import pygments.lexers
 
 import db
 import dex.cache
+import dex.csv_parser
 import dex.csv_tmp
+import dex.eml_types
 import dex.eml_types
 import dex.exc
 import dex.pasta
+import util
 
 """
 * default:
@@ -69,13 +64,11 @@ PYGMENTS_STYLE_LIST = [
 ]
 # fmt:on
 
-EML_STYLE_NAME = 'perldoc'
-
 log = logging.getLogger(__name__)
 
 # Default start and end datetimes used in the UI if the EML lists one or more datetime
 # columns, but no datetime ranges.
-FALLBACK_START_DATETIME = datetime.datetime(
+FALLBACK_BEGIN_DATETIME = datetime.datetime(
     2000,
     1,
     1,
@@ -96,90 +89,22 @@ DATE_FORMAT_DICT = {
 }
 
 
-def get_attribute_fragments(rid):
-    """Get column descriptions for a given CSV file. The columns are described in EML
-    attribute elements.
-    """
-    ss = io.StringIO()
-    dt_el = find_data_table(rid)
-    object_name = first_str(dt_el, './/physical/objectName/text()')
-    ss.write(f'\n{object_name}\n\n')
-
-    for i, attr_el in enumerate(dt_el.xpath('.//attributeList/attribute')):
-        ss.write(pretty_format_fragment(attr_el))
-
-    return ss.getvalue()
-
-
-def get_profiling_types(rid):
-    dt_el = find_data_table(rid)
-    return dex.eml_types.get_profiling_types(dt_el)
-
-
-def has(el, xpath):
-    return first(el, xpath) is not None
-
-
-def first(el, xpath):
-    """Return the first match to the xpath if there was a match, else None. Can this be
-    done directly in xpath 1.0?
-    """
-    log.debug(f'first() xpath={xpath} ...')
-    res_el = el.xpath(f'({xpath})[1]')
-    try:
-        el = res_el[0]
-    except IndexError:
-        el = None
-    log.debug(f'first() -> {el}')
-    return el
-
-
-def first_str(el, text_xpath):
-    """Apply xpath and, if there is a match, assume that the match is a text node, and
-    convert it to an uppper case string. {text_xpath} is an xpath that returns a text
-    node. E.g., `.//text()`.
-    """
-    el = first(el, text_xpath)
-    s = None
-    if el:
-        s = str(el).upper().strip()
-    log.debug(f'first_str() -> {s}')
-    return s
-
-
-def get_iso_date_time(iso_str):
-    """Try to parse as ISO8601. Return a string on form 'YYYY-MM-DD' if parsing is
-    successful, else None.
-    """
-    with contextlib.suppress(ValueError, TypeError):
-        return dateutil.parser.isoparse(iso_str).strftime('%Y-%m-%d')
-
-
 # @dex.cache.disk('eml-dt-cols', 'list')
 def get_datetime_columns(rid):
-    dt_el = find_data_table(rid)
-    default_dt = get_default_start_end_datetime_range(rid)
-    # attributeList/attribute contains descriptions of the columns in the CSV
+    type_list = dex.csv_parser.get_derived_dtypes_from_eml(rid)
     dt_col_list = []
-    for i, attr_el in enumerate(dt_el.xpath('.//attributeList/attribute')):
-        if first_str(attr_el, './/storageType/text()') not in ('dateTime', 'date'):
-            continue
-        dt_format_str = first_str(
-            attr_el, '//measurementScale/dateTime/formatString/text()'
-        )
-        begin_dt = get_iso_date_time(
-            first_str(attr_el, './/beginDate/calendarDate/text()')
-        )
-        end_dt = get_iso_date_time(first_str(attr_el, './/endDate/calendarDate/text()'))
-        dt_col_list.append(
-            dict(
-                col_idx=i,
-                dt_format_str=dt_format_str,
-                begin_dt=begin_dt or default_dt.start_dt,
-                end_dt=end_dt or default_dt.end_dt,
-            )
-        )
-    log.debug(f'Found datetime columns: {dt_col_list}')
+    for col_idx, dtype_dict in enumerate(type_list):
+        if dtype_dict['type_str'] == 'TYPE_DATE':
+            dt_col_list.append(dtype_dict)
+    return dt_col_list
+
+
+def get_categorical_columns(rid):
+    type_list = dex.csv_parser.get_derived_dtypes_from_eml(rid)
+    dt_col_list = []
+    for col_idx, dtype_dict in enumerate(type_list):
+        if dtype_dict['type_str'] == 'TYPE_CAT':
+            dt_col_list.append(dtype_dict)
     return dt_col_list
 
 
@@ -187,70 +112,74 @@ def get_datetime_columns_as_dict(rid):
     return {d['col_idx']: d for d in get_datetime_columns(rid)}
 
 
-def get_default_start_end_datetime_range(rid):
-    el = get_eml_tree(rid)
-    return types.SimpleNamespace(
-        start_dt=get_iso_date_time(
-            first_str(
-                el,
-                './/dataset/coverage/temporalCoverage/rangeOfDates/beginDate/calendarDate/text()',
-            )
-        )
-        or FALLBACK_START_DATETIME,
-        end_dt=get_iso_date_time(
-            first_str(
-                el,
-                './/dataset/coverage/temporalCoverage/rangeOfDates/endDate/calendarDate/text()',
-            )
-        )
-        or FALLBACK_END_DATETIME,
-    )
+# etree formatting
 
 
-def find_data_table(rid):
-    el = get_eml_tree(rid)
-    data_url = db.get_data_url(rid).upper()
-    data_url = data_url[data_url.find('/PACKAGE/') :]
-    for dt_el in el.xpath('.//dataset/dataTable'):
-        url = first_str(dt_el, './/physical/distribution/online/url/text()')
-        url = url[url.find('/PACKAGE/') :]
-        if url == data_url:
-            return dt_el
-    raise dex.exc.EMLError(f'Missing DataTable in EML. rid="{rid}"')
+def get_attributes_as_highlighted_html(rid):
+    """Get column descriptions for a given CSV file. The columns are described in EML
+    attribute elements.
+    """
+    return {
+        k: util.get_etree_as_highlighted_html(v)
+        for k, v in get_attributes_as_etree(rid).items()
+    }
 
+
+@dex.cache.disk('eml-highlighted', 'pickle')
+def get_eml_as_highlighted_html(rid):
+    root_el = get_eml_etree(rid)
+    try:
+        root_el = root_el.getroottree()
+    except AttributeError:
+        pass
+    return util.get_etree_as_highlighted_html(root_el)
+
+
+# Read from the EML doc
+
+# Read the EML attribute fragments that declare the types and other information for each
+# column in the CSV documents.
 
 # noinspection PyUnresolvedReferences
-@dex.cache.disk('eml-highlighted', 'xml')
-def get_eml_highlighted_html(rid):
-    html_formatter = pygments.formatters.HtmlFormatter(style=EML_STYLE_NAME)
-    xml_str = get_eml_xml(rid)
-    return (
-        pygments.highlight(xml_str, pygments.lexers.XmlLexer(), html_formatter),
-        html_formatter.get_style_defs('.highlight'),
-    )
+# @dex.cache.disk('attributes-tree', 'etree')
+# TODO: Find best way to cache dict of etree.
+def get_attributes_as_etree(rid):
+    dt_el = get_data_table(rid)
+    return {
+        # Using this pattern, of creating a dict with the column index as the key
+        # since we can't be sure that the attributes will always be stored in column
+        # order in the EML doc.
+        col_idx: attr_el
+        for col_idx, attr_el in enumerate(dt_el.xpath('.//attributeList/attribute'))
+    }
+
+
+def get_csv_name(dt_el):
+    return dex.eml_types.first_str(dt_el, './/physical/objectName/text()')
+
+
+@dex.cache.disk('datatable', 'etree')
+def get_data_table(rid):
+    entity_tup = db.get_entity(rid)
+    pkg_path = dex.pasta.get_pkg_id_as_url(entity_tup)
+    eml_el = get_eml_etree(rid)
+    dt_el = dex.eml_types.get_data_table_by_package_id(eml_el, pkg_path)
+    return dt_el
+
+
+# Full EML
 
 
 @dex.cache.disk('eml', 'xml')
 def get_eml_xml(rid):
-    root_el = get_eml_tree(rid)
-    return pretty_format_fragment(root_el)
+    root_el = get_eml_etree(rid)
+    return util.get_etree_as_pretty_printed_xml(root_el)
 
 
-M @ dex.cache.disk('eml', 'etree')
-
-
-def get_eml_tree(rid):
-    if isinstance(rid, pathlib.Path):
-        eml_path = rid
-    else:
-        eml_path = dex.csv_tmp.get_eml_path_by_row_id(rid)
+@dex.cache.disk('eml', 'etree')
+def get_eml_etree(rid):
+    # if isinstance(rid, pathlib.Path):
+    #     eml_path = rid
+    # else:
+    eml_path = dex.csv_tmp.get_eml_path_by_row_id(rid)
     return lxml.etree.parse(eml_path.as_posix())
-
-
-def pretty_format_fragment(el):
-    if not isinstance(el, list):
-        el = [el]
-    buf = io.BytesIO()
-    for e in el:
-        buf.write(lxml.etree.tostring(e, pretty_print=True))
-    return buf.getvalue().decode('utf-8')
