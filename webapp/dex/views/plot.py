cee06f246719f741d87b5d0068620759fb5805bf
Roger Dahl <git@dahlsys.com>
2021-05-13 12:27:30 -0600

    Add support for dtypes derived from EML in Plot page

--- webapp/dex/views/plot.py
+++ webapp/dex/views/plot.py
@@ -1,12 +1,13 @@
 import logging
 
 import flask
-import numpy
-import pandas
 
 import db
 import dex.csv_cache
+import dex.csv_parser
+import dex.debug
 import dex.eml_cache
+import util
 
 log = logging.getLogger(__name__)
 
@@ -14,57 +15,51 @@ plot_blueprint = flask.Blueprint("plot", __name__, url_prefix="/dex/plot")
 
 
 @plot_blueprint.route("/<rid>")
-def profile(rid):
-    # rid = db.get_entity(rid)
-
-    # csv_df = csv_cache.get_full_csv(rid)
-    desc_df = dex.csv_cache.get_description(rid)
-
-    pandas.set_option(
-        "display.max_rows",
-        100,
-        "display.max_columns",
-        None,
-        "display.max_colwidth",
-        10000,
-        "display.width",
-        10000,
-    )
-
-    dt_col_dict = dex.eml_cache.get_datetime_columns_as_dict(rid)
-    dt_col_set = set(dt_col_dict.keys())
+def plot(rid):
+    """DATE is only on X
+    NUM is on X and Y
+    CAT is not on either axis
+    UNSUPPORTED is not on either axis
+    """
+    ctx = dex.csv_parser.get_parsed_csv_with_context(rid)
 
     sel_list_x = []
     sel_list_y = []
 
-    for i, col_name in enumerate(desc_df.columns):
-        c = desc_df.iloc[:, i]
-        if i in dt_col_set:
-            sel_str = f'{col_name} (Time Series, {dt_col_dict[i]["begin_dt"]} - {dt_col_dict[i]["end_dt"]})'
-            sel_list_x.append((sel_str, i))
-        elif c.dtype == numpy.number:
-            sel_str = f'{col_name} (min={c["min"]:,.02f} max={c["max"]:,.02f})'
-            sel_list_x.append((sel_str, i))
-            sel_list_y.append((sel_str, i))
-        else:
-            # Skip categorical data. TODO: Do we want to include categories if they're numerical, and so can be plotted?
-            pass
-            # sel_str = f'{col_name} (Categorical)'
-            # sel_list_x.append((sel_str, i))
-            # sel_list_y.append((sel_str, i))
+    for col_idx, dtype_dict in enumerate(ctx['derived_dtypes_list']):
+        d = N(**dtype_dict)
 
-    return flask.render_template(
-        "plot.html",
+        agg_dict = ctx['col_agg_dict'].get(col_idx)
+        if agg_dict is None:
+            continue
+
+        friendly_type = dex.csv_parser.DTYPE_TO_FRIENDLY_DICT[d.type_str]
+        # agg_dict = ctx['col_agg_dict'][col_idx]
+        sel_str = '{} ({}, {} - {})'.format(
+            d.col_name, friendly_type, agg_dict['v_min'], agg_dict['v_max']
+        )
+        if d.type_str == 'TYPE_DATE':
+            sel_list_x.append((sel_str, col_idx))
+        elif d.type_str == 'TYPE_NUM':
+            sel_list_x.append((sel_str, col_idx))
+            sel_list_y.append((sel_str, col_idx))
+
+    g_dict = dict(
         rid=rid,
         entity_tup=db.get_entity_as_dict(rid),
         cols_x=sel_list_x,
         cols_y=sel_list_y,
     )
 
-    # unique_list = sorted(csv_df[col_name].unique().tolist())
-    # if (
-    #     len(unique_list) / len(csv_df)
-    #     > (flask.current_app.config['CATEGORY_THRESHOLD_PERCENT'] / 100)
-    # ):
-    #     return json.dumps([])
-    # return json.dumps(unique_list)
+    util.logpp(g_dict, 'g_dict', log.debug)
+
+    return flask.render_template(
+        "plot.html",
+        g_dict=g_dict,
+        cols_x=sel_list_x,
+        cols_y=sel_list_y,
+        # For the base template, should be included in all render_template() calls.
+        rid=rid,
+        entity_tup=db.get_entity_as_dict(rid),
+        dbg=dex.debug.debug(rid),
+    )
