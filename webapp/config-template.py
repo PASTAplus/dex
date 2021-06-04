ba97ec352e0cc2956263a4e7995b5f9325b40910
Roger Dahl <git@dahlsys.com>
2021-06-04 09:36:23 -0600

    Remove settings for heuristics obsoleted by move th EML types

--- webapp/config-template.py
+++ webapp/config-template.py
@@ -1,7 +1,7 @@
 import pathlib
 import sys
 
-FLASK_DEBUG = True
+FLASK_DEBUG = False
 
 SESSION_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
@@ -14,7 +14,7 @@ try:
 except ImportError:
     RUNNING_UNDER_UWSGI = False
 
-# For debugging, disk caching can be disabled. The cached versions will still be updated.
+# For debugging, disk caching can be disabled.
 DISK_CACHE_ENABLED = False
 
 ROOT_PATH = pathlib.Path(__file__, "../..").resolve()
@@ -28,9 +28,6 @@ if RUNNING_UNDER_UWSGI:
     PYTHON_BIN = "/home/pasta/anaconda3/envs/dex2/bin/python"
 else:
     PYTHON_BIN = sys.executable
-
-PROFILING_BIN = ROOT_PATH / "webapp/profiling_proc.py"
-PROFILING_SH = ROOT_PATH / "webapp/profiling_proc.sh"
 
 SECRET_KEY = "SECRET_KEY"
 
@@ -49,17 +46,10 @@ STALE = 10
 # If these values are changed, the cached dataframes must be cleared for the new values
 # to take effect.
 
-# The percentage of unique values in a table column must be below this value in order
-# for the column to be considered to be holding categorical data.
-CATEGORY_THRESHOLD_PERCENT = 20
-# The percentage of values in a table column which must be parsable as a date-time in
-# order for the column to be considered to be holding date-times.
-DATETIME_THRESHOLD_PERCENT = 50
-
 # Threshold at which we switch from processing all rows in a CSV file and instead
 # process only a sample of the rows. Effectively, the number of rows that are processed
 # for most functionality is capped at this value.
 CSV_SAMPLE_THRESHOLD = 10000
 
 # Max number of rows to examine when analyzing CSV files to determine format and content.
-CSV_SNIFF_THRESHOLD = 1000
+CSV_SNIFFER_THRESHOLD = 1000
