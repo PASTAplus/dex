a2ad6660f6e5b57884d6c4d57ef392cdbd95d621
Roger Dahl <git@dahlsys.com>
2021-09-13 16:23:53 -0600

    Add support for ISO8601-style format specifications

--- dex/eml_types.py
+++ dex/eml_types.py
@@ -41,6 +41,128 @@ DATE_INT_FORMAT_DICT = {
 }
 
 
+ISO8601_TO_CTIME_DICT = {
+    # %a
+    # Weekday as locale’s abbreviated name.
+    # Sun, Mon, …, Sat (en_US);
+    'DDD': '%a',
+    'w': '%a',
+    # %A
+    # Weekday as locale’s full name.
+    # Sunday, Monday, …, Saturday (en_US);
+    'W': '%a',
+    # %w
+    # Weekday as a decimal number, where 0 is Sunday and 6 is Saturday.
+    # 0, 1, …, 6
+    'WW': '%w',
+    # %d
+    # Day of the month as a zero-padded decimal number.
+    # 01, 02, …, 31
+    'DD': '%d',
+    'dd': '%d',
+    # %b
+    # Month as locale’s abbreviated name.
+    # Jan, Feb, …, Dec (en_US);
+    'MON': '%b',
+    'mon': '%b',
+    # %B
+    # Month as locale’s full name.
+    # January, February, …, December (en_US);
+    # %y
+    # Year without century as a zero-padded decimal number.
+    # 00, 01, …, 99
+    'YY': '%y',
+    'yy': '%y',
+    # %Y
+    # Year with century as a decimal number.
+    # 0001, 0002, …, 2013, 2014, …, 9998, 9999
+    'YYYY': '%Y',
+    'yyyy': '%Y',
+    # %H
+    # Hour (24-hour clock) as a zero-padded decimal number.
+    # 00, 01, …, 23
+    'HH': '%H',
+    'HH24': '%H',
+    'hh': '%H',
+    'h': '%H',
+    # %I
+    # Hour (12-hour clock) as a zero-padded decimal number.
+    # 01, 02, …, 12
+    # %p
+    # Locale’s equivalent of either AM or PM.
+    # AM, PM (en_US);
+    # %M
+    # Minute as a zero-padded decimal number.
+    # 00, 01, …, 59
+    'MM': '%m',
+    'mm': '%M',
+    'm': '%M',
+    # %S
+    # Second as a zero-padded decimal number.
+    # 00, 01, …, 59
+    'SS': '%S',
+    'ss': '%S',
+    's': '%M',
+    # %f
+    # Microsecond as a decimal number, zero-padded on the left.
+    # 000000, 000001, …, 999999
+    #
+    # %z
+    # UTC offset in the form ±HHMM[SS[.ffffff]] (empty string if the object is naive).
+    # (empty), +0000, -0400, +1030, +063415, -030712.345216
+    #
+    # %Z
+    # Time zone name (empty string if the object is naive).
+    # (empty), UTC, GMT
+    #
+    # %j
+    # Day of the year as a zero-padded decimal number.
+    # 001, 002, …, 366
+    'DDDD': '%j',
+    # %U
+    # Week number of the year (Sunday as the first day of the week) as a zero padded decimal number. All days in a new year preceding the first Sunday are considered to be in week 0.
+    # 00, 01, …, 53
+    #
+    # %W
+    # Week number of the year (Monday as the first day of the week) as a decimal number. All days in a new year preceding the first Monday are considered to be in week 0.
+    # 00, 01, …, 53
+    #
+    # %c
+    # Locale’s appropriate date and time representation.
+    # Tue Aug 16 21:30:00 1988 (en_US);
+    #
+    # %x
+    # Locale’s appropriate date representation.
+    # 08/16/1988 (en_US);
+    #
+    # Locale’s appropriate time representation.
+    # 21:30:00 (en_US);
+    # %m
+    # Month as a zero-padded decimal number.
+    # 01, 02, …, 12
+}
+
+
+def iso8601_to_c_format(iso_str):
+    """Convert an ISO8601-style datetime format spec, as used in EML, to a C-style
+    format spec, as used by the Python datetime formatting functions.
+    """
+    c_list = []
+    for x in re.split(
+        '(\W|integer|alphabetic|[T]|MST|YYYY|YY|MM|DDD|DD|HH|HH24|SS|mon)',
+        iso_str,
+        flags=re.IGNORECASE,
+    ):
+        if x:
+            c_str = ISO8601_TO_CTIME_DICT.get(x, x)
+            c_list.append(c_str)
+            # log.debug(f'Translating datetime format string. ISO8601="{x}" C="{c_str}"')
+
+    c_fmt_str = ''.join(c_list)
+    log.debug(f'Translated datetime format string. ISO8601="{iso_str}" C="{c_fmt_str}"')
+    return c_fmt_str
+
+
 def get_derived_dtypes_from_eml(dt_el):
     """Heuristics to find a Pandas / NumPy type, called dtype, to use when processing
     a CSV file that has EML based type declarations.
@@ -147,11 +269,10 @@ def get_derived_dtypes_from_eml(dt_el):
                 try_date_time_parse(end_date_str) if end_date_str else default_dt.end_dt
             )
 
-        elif date_fmt_str in DATE_FORMAT_DICT.keys():
+        if date_fmt_str:
             dtype_dict['type_str'] = 'TYPE_DATE'
             dtype_dict['date_fmt_str'] = date_fmt_str
-            date_fmt_str = DATE_FORMAT_DICT[date_fmt_str]
-            dtype_dict['c_date_fmt_str'] = date_fmt_str
+            dtype_dict['c_date_fmt_str'] = iso8601_to_c_format(date_fmt_str)
             begin_date_str = first_str(attr_el, './/beginDate/calendarDate/text()')
             end_date_str = first_str(attr_el, './/endDate/calendarDate/text()')
             dtype_dict['begin_dt'] = (
