2d90a7cf9b79490c2b40c37f8ed7e758a2d0a6ec
Roger Dahl <git@dahlsys.com>
2021-09-30 13:15:38 -0600

    Simplify code by moving to a single type of CSV representation

--- dex/csv_cache.py
+++ dex/csv_cache.py
@@ -4,6 +4,7 @@ import time
 
 import pandas as pd
 
+import csv_parser
 import dex.cache
 import dex.csv_parser
 import dex.obj_bytes
@@ -18,31 +19,14 @@ from flask import current_app as app
 log = logging.getLogger(__name__)
 
 
-@dex.cache.disk("full", "df")
-def get_full_csv(rid, **csv_arg_dict):
-    try:
-        df = _load_csv_to_df(rid, **csv_arg_dict)
-        dex.csv_parser.cast_to_eml_types(df, rid)
-        return df
-    except FileNotFoundError:
-        raise dex.exc.RedirectToIndex()
-
-
-@dex.cache.disk("full-cast", "df")
-def get_full_csv_with_eml_cast(rid, **csv_arg_dict):
-    df = get_full_csv(rid, **csv_arg_dict)
-    dex.csv_parser.cast_to_eml_types(df, rid)
-    return df
-
-
 @dex.cache.disk("head", "df")
 def get_csv_head(rid):
-    return get_full_csv(rid).head()
+    return csv_parser.get_parsed_csv(rid).head()
 
 
 @dex.cache.disk("tail", "df")
 def get_csv_tail(rid):
-    return get_full_csv(rid).tail()
+    return csv_parser.get_parsed_csv(rid).tail()
 
 
 @dex.cache.disk("sample", "df")
@@ -127,40 +111,6 @@ def get_categories_for_column(rid, col_idx):
     return list(x for x in col_series.unique() if not is_nan(x))
 
 
-#
-
-
-def _load_csv_to_df(rid, **csv_arg_dict):
-    """Read CSV file to DataFrame.
-
-    This uses CleverCSV to determine the dialect of the CSV, then reads it with the
-    standard csv module to save time.
-    """
-    csv_stream = dex.obj_bytes.open_csv(rid)
-    dialect = dex.csv_parser.get_dialect(csv_stream)
-    skip_rows, header_row = dex.csv_parser.find_header_row(csv_stream)
-    log.debug(f"skip_rows: {skip_rows}")
-    log.debug(f"pandas.read_csv start: {csv_stream}")
-    start_ts = time.time()
-
-    csv_arg_dict.pop("skiprows", None)
-
-    csv_df = pd.read_csv(
-        csv_stream,
-        dialect=dialect,
-        parse_dates=True,
-        # parse_dates=['TS'],
-        # date_parser=parse_date
-        # nrows=1000,
-        skiprows=skip_rows,
-        **csv_arg_dict,
-    )
-
-    log.debug(f"pandas.read_csv: {time.time() - start_ts:.02f}s")
-    # log.debug(f"Memory used by DataFrame: {csv_df.memory_usage(deep=True)}")
-    return csv_df
-
-
 def get_dt_df(df):
     """Return a new DataFrame containing only the columns from `df` that contain
     date-times. Columns containing mixed values where more than `fuzz_percent`
@@ -178,24 +128,3 @@ def get_dt_df(df):
             df[col_name] = col
 
     return dt_df
-
-
-# @dex.cache.disk("dt-col", "df")
-# def get_dt_col(rid, col_idx):
-#     """Return the column with index `col_idx` as a date-time series.
-#
-#     In mixed value columns, the NaT (not a time) rows are filled in with interpolated
-#     date-times.
-#     """
-#     df = get_csv_sample(rid)
-#     dt_series = df.iloc[:, col_idx]
-#     dt_series = pd.to_datetime(dt_series, errors="coerce", format=)
-#     dt_series.fillna(method=  "ffill")
-#     return dt_series
-
-
-# def get_mixed_columns(df):
-#     """Return indexes of columns that have mixed types or missing values"""
-#     return [
-#         col.index for col, col_type in df.dtypes.iteritems() if col_type == "object"
-#     ]
