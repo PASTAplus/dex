2c862c5041b67a42faa5f2d9b60f680760b18f7a
Roger Dahl <git@dahlsys.com>
2022-02-06 23:12:38 -0700

    Add disclaimers in Profile

--- dex/views/profile.py
+++ dex/views/profile.py
@@ -2,6 +2,8 @@ import logging
 
 import flask
 import flask.json
+from flask import current_app as app
+
 import pandas_profiling
 
 import dex.db
@@ -21,10 +23,19 @@ profile_blueprint = flask.Blueprint("profile", __name__, url_prefix="/dex/profil
 # noinspection PyUnresolvedReferences
 @profile_blueprint.route("/<rid>")
 def profile(rid):
+    csv_df, raw_df, eml_ctx = dex.csv_parser.get_parsed_csv_with_context(rid)
+
+    note_list = []
+    if len(csv_df) == app.config['CSV_MAX_ROWS']:
+       note_list.append('Due to size, only the first part of this table is available in DeX')
+
+    note_list.append('This analysis is not based on information from the EML metadata')
+
     return flask.render_template(
         "profile.html",
         g_dict=dict(
             is_cached=dex.cache.is_cached(rid, "profile", "html"),
+            rid=rid,
         ),
         # For the base template, should be included in all render_template() calls.
         rid=rid,
@@ -32,6 +43,7 @@ def profile(rid):
         csv_name=dex.eml_cache.get_csv_name(rid),
         dbg=dex.debug.debug(rid),
         portal_base=dex.pasta.get_portal_base_by_entity(dex.db.get_entity(rid)),
+        note_list=note_list,
     )
 
 
@@ -39,8 +51,8 @@ def profile(rid):
 def doc(rid):
     """Return the Pandas Profiling HTML doc for the given rid. If the profile has not
     been generated, this holds the connection until the doc is ready, then returns it."""
-    if not dex.cache.is_cached(rid, "profile", "html"):
-        render_profile(rid)
+    # if not dex.cache.is_cached(rid, "profile", "html"):
+    render_profile(rid)
 
     def chunks_gen():
         with dex.cache.open_file(rid, 'profile', 'html') as f:
@@ -68,6 +80,18 @@ def render_profile(rid):
         config_file=flask.current_app.config['PROFILING_CONFIG_PATH'],
         # dark_mode=arg_dict['dark_mode'],
         infer_dtypes=False,
+        # correlation_overrides=csv_df.columns.tolist(),
+        # correlation_threshold=1,
+        # check_correlation=False,
+        correlations={
+            # "pearson": False,
+            # "spearman": False,
+            # "kendall": False,
+            # "phi_k": False,
+            # "cramers": False,
+            # "recoded": False,
+        },
+        # plot={'histogram': {'bins': None}},
     )
     rearrange_report(report_tree)
     html_str = report_tree.to_html()
@@ -78,10 +102,14 @@ def rearrange_report(report_tree):
     try:
         # Move the Sample section from the end to the front of the report.
         section_list = report_tree.report.content["body"].content["items"]
-        section_list.insert(1, section_list.pop(-1))
+        try:
+            section_list.insert(1, section_list.pop(-1))
+        except Exception:
+            log.exception('Unable to reorder the sections of the Pandas Profiling report')
 
-        section_list[0].content['items'][1].content['name'] = 'Notes'
-        section_list[0].content['items'][2].content['name'] = 'Reproducibility'
-        section_list[0].content['items'][2].content['items'][0].content['name'] = 'Reproducibility'
+        items = section_list[0].content['items']
+        items[1].content['name'] = 'Notes'
+        items[2].content['name'] = 'Report Reproducibility'
+        items[2].content['items'][0].content['name'] = 'Report Reproducibility'
     except Exception:
-        log.exception('Unable to reorder the sections of the Pandas Profiling report')
+        log.exception('Unable to rename parts of the Pandas Profiling report')
