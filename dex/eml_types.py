81b4823c2fe514420edffc01bcca20586f33bd10
Roger Dahl <git@dahlsys.com>
2022-02-06 22:40:35 -0700

    Add more complete and simplified ISO to C date format conversion

--- dex/eml_types.py
+++ dex/eml_types.py
@@ -46,122 +46,178 @@ PANDAS_TO_FRIENDLY_DICT = {
 
 
 ISO8601_TO_CTIME_DICT = {
-    # %a
+    # This dict was created based on an analysis of the full LTER and EDI corpus of
+    # CSV files.
+    # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+    # -- %a
     # Weekday as locale’s abbreviated name.
     # Sun, Mon, …, Sat (en_US);
-    'DDD': '%a',
+    # So, Mo, …, Sa (de_DE)
     'w': '%a',
-    # %A
+    'W': '%a',
+    'DDD': '%a',
+    'WWW': '%a',
+    # -- %A
     # Weekday as locale’s full name.
     # Sunday, Monday, …, Saturday (en_US);
-    'W': '%a',
-    # %w
+    # Sonntag, Montag, …, Samstag (de_DE)
+    # -- %w
     # Weekday as a decimal number, where 0 is Sunday and 6 is Saturday.
     # 0, 1, …, 6
     'WW': '%w',
-    # %d
+    # -- %d
     # Day of the month as a zero-padded decimal number.
     # 01, 02, …, 31
     'DD': '%d',
     'dd': '%d',
-    # %b
+    'D': '%d',
+    # -- %b
     # Month as locale’s abbreviated name.
     # Jan, Feb, …, Dec (en_US);
+    # Jan, Feb, …, Dez (de_DE)
+    'Month': '%b',
+    'mmm': '%b',
+    'MMM': '%b',
     'MON': '%b',
     'mon': '%b',
-    # %B
+    # -- %B
     # Month as locale’s full name.
     # January, February, …, December (en_US);
-    # %y
+    # Januar, Februar, …, Dezember (de_DE)
+    # -- %m
+    # Month as a zero-padded decimal number.
+    # 01, 02, …, 12
+    'MM': '%m',
+    # -- %y
     # Year without century as a zero-padded decimal number.
     # 00, 01, …, 99
+    'YYY': '%y',
     'YY': '%y',
     'yy': '%y',
-    # %Y
+    # -- %Y
     # Year with century as a decimal number.
     # 0001, 0002, …, 2013, 2014, …, 9998, 9999
     'YYYY': '%Y',
     'yyyy': '%Y',
-    # %H
+    # -- %H
     # Hour (24-hour clock) as a zero-padded decimal number.
     # 00, 01, …, 23
     'HH': '%H',
     'HH24': '%H',
+    'hh24': '%H',
     'hh': '%H',
     'h': '%H',
-    # %I
+    # -- %I
     # Hour (12-hour clock) as a zero-padded decimal number.
     # 01, 02, …, 12
-    # %p
+    # -- %p
     # Locale’s equivalent of either AM or PM.
     # AM, PM (en_US);
-    # %M
+    # am, pm (de_DE)
+    # -- %M
     # Minute as a zero-padded decimal number.
     # 00, 01, …, 59
-    'MM': '%m',
+    'MI': '%M',
+    'mi': '%M',
     'mm': '%M',
     'm': '%M',
-    # %S
+    # -- %S
     # Second as a zero-padded decimal number.
-    # 00, 01, …, 59
     'SS': '%S',
     'ss': '%S',
-    's': '%M',
-    # %f
-    # Microsecond as a decimal number, zero-padded on the left.
+    's': '%S',
+    # -- %f
+    # Microsecond as a decimal number, zero-padded to 6 digits.
     # 000000, 000001, …, 999999
-    #
-    # %z
+    # -- %z
     # UTC offset in the form ±HHMM[SS[.ffffff]] (empty string if the object is naive).
     # (empty), +0000, -0400, +1030, +063415, -030712.345216
-    #
-    # %Z
+    '\+hhmm': '%z',
+    # -- %Z
     # Time zone name (empty string if the object is naive).
     # (empty), UTC, GMT
-    #
-    # %j
+    'UTC': '%Z',
+    'GMT': '%Z',
+    # -- %j
     # Day of the year as a zero-padded decimal number.
     # 001, 002, …, 366
+    # 'DDD': '%j',
     'DDDD': '%j',
-    # %U
-    # Week number of the year (Sunday as the first day of the week) as a zero padded decimal number. All days in a new year preceding the first Sunday are considered to be in week 0.
+    # -- %U
+    # Week number of the year (Sunday as the first day of the week) as a zero-padded
+    # decimal number. All days in a new year preceding the first Sunday are considered
+    # to be in week 0.
     # 00, 01, …, 53
-    #
-    # %W
-    # Week number of the year (Monday as the first day of the week) as a decimal number. All days in a new year preceding the first Monday are considered to be in week 0.
+    # -- %W
+    # Week number of the year (Monday as the first day of the week) as a zero-padded
+    # decimal number. All days in a new year preceding the first Monday are considered
+    # to be in week 0.
     # 00, 01, …, 53
-    #
-    # %c
+    # -- %c
     # Locale’s appropriate date and time representation.
     # Tue Aug 16 21:30:00 1988 (en_US);
-    #
-    # %x
+    # Di 16 Aug 21:30:00 1988 (de_DE)
+    # -- %x
     # Locale’s appropriate date representation.
+    # 08/16/88 (None);
     # 08/16/1988 (en_US);
-    #
+    # 16.08.1988 (de_DE)
+    # -- %X
     # Locale’s appropriate time representation.
     # 21:30:00 (en_US);
-    # %m
-    # Month as a zero-padded decimal number.
-    # 01, 02, …, 12
+    # 21:30:00 (de_DE)
+    # -- %%
+    # A literal '%' character.
+    # -- %
+    # -- %G
+    # ISO 8601 year with century representing the year that contains the greater part of
+    # the ISO week (%V).
+    # 0001, 0002, …, 2013, 2014, …, 9998, 9999
+    # -- %u
+    # ISO 8601 weekday as a decimal number where 1 is Monday.
+    # 1, 2, …, 7
+    # -- %V
+    # ISO 8601 week as a decimal number with Monday as the first day of the week. Week
+    # 01 is the week containing Jan 4.
+    # 01, 02, …, 53
+    # Weekday as locale’s full name.
+    # Sunday, Monday, …, Saturday (en_US);
+    # Weekday as a decimal number, where 0 is Sunday and 6 is Saturday.
+    # 0, 1, …, 6
+    # Literals to preserve
+    'Z': 'Z',
 }
 
 
 def iso8601_to_c_format(iso_str):
     """Convert an ISO8601-style datetime format spec, as used in EML, to a C-style
     format spec, as used by the Python datetime formatting functions.
+
+    Args:
+        iso_str (str):
+            ISO8601-style datetime format spec from EML
+
+    Returns:
+        str: C-style format spec, as used by the Python datetime formatting functions
+        None: Returned if 'iso_str' contains one or more sequences that we are unable to
+        translate.
     """
     c_list = []
-    for x in re.split(
-        '(\W|integer|alphabetic|[T]|MST|YYYY|YY|MM|DDD|DD|HH|HH24|SS|mon)',
-        iso_str,
-        flags=re.IGNORECASE,
-    ):
-        if x:
-            c_str = ISO8601_TO_CTIME_DICT.get(x, x)
+    # We look for keys in longest to shortest order, with key string itself as tie breaker.
+    key_list = list(sorted(ISO8601_TO_CTIME_DICT.keys(), key=lambda s: (-len(s), s)))
+    rx_str = '|'.join(key_list)
+    rx_str = f'({rx_str})'
+    for iso_str in re.split(rx_str, iso_str, flags=re.IGNORECASE):
+        if iso_str:
+            try:
+                c_str = ISO8601_TO_CTIME_DICT[iso_str]
+            except KeyError:
+                if iso_str in list(' _-/,:T{}()[]'):
+                    c_str = iso_str
+                else:
+                    return
             c_list.append(c_str)
-            # log.debug(f'Translating datetime format string. ISO8601="{x}" C="{c_str}"')
-
+            # log.debug(f'Translating datetime format string. ISO8601="{iso_str}" C="{c_str}"')
     c_fmt_str = ''.join(c_list)
     log.debug(f'Translated datetime format string. ISO8601="{iso_str}" C="{c_fmt_str}"')
     return c_fmt_str
